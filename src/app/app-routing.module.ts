dfd7255e90b82f31e653fbf3dbc0a3831daaebb5
hem2182 <hemant_2182_fresco@yahoo.com>
2020-03-13 19:28:18 +0530

    Initial Setup of client application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MedicinesComponent } from './medicines/medicines.component';
+import { AddMedicinesComponent } from './add-medicines/add-medicines.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'medicines', component: MedicinesComponent },
+  { path: 'add', component: AddMedicinesComponent },
+  { path: '', component: MedicinesComponent },
+  { path: '*', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MedicinesComponent } from './medicines/medicines.component';
+import { AddMedicinesComponent } from './add-medicines/add-medicines.component';
+import { MedicineService } from './Services/medicine.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicinesComponent,
+    AddMedicinesComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MedicineService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
